babdb818490cc9895f74d766ea6a5b0bd2f546e8
JesseTerHorst <71325876+JesseTerHorst@users.noreply.github.com>
2021-10-05 15:07:54 +0200

    Update MainWindow.xaml.cs

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -25,13 +25,58 @@ namespace Project
             InitializeComponent();
         }
 
-        static private bool isSpeedBoostActive;
-        static void ActiveSpeedBoost()
+        //static private bool isSpeedBoostActive;
+        //static void ActiveSpeedBoost()
+        //{
+        //    if (isSpeedBoostActive)
+        //    {
+        //        isSpeedBoostActive = true;
+        //        speed += 5;
+        //    }
+        //}
+
+
+        public Window1() 
         {
-            if (isSpeedBoostActive)
+            // Instantiate Food Objects
+            for (int n = 0; n < 10; n++)
             {
-                isSpeedBoostActive = true;
-                speed += 5;
+                paintBonus(n);
+            }
+        }
+        private void paintBonus(int index)
+        {
+            Point bonusPoint = new Point(rnd.Next(5, 620), rnd.Next(5, 380));
+
+            Ellipse newEllipse = new Ellipse();
+            newEllipse.Fill = Brushes.Red;
+            newEllipse.Width = headSize;
+            newEllipse.Height = headSize;
+
+            Canvas.SetTop(newEllipse, bonusPoint.Y);
+            Canvas.SetLeft(newEllipse, bonusPoint.X);
+            paintCanvas.Children.Insert(index, newEllipse);
+            bonusPoints.Insert(index, bonusPoint);
+
+            // Hitting a bonus Point causes the lengthen-Snake Effect
+            int n = 0;
+            foreach (Point point in bonusPoints)
+            {
+
+                if ((Math.Abs(point.X - currentPosition.X) < headSize) &&
+                    (Math.Abs(point.Y - currentPosition.Y) < headSize))
+                {
+                    length += 10;
+                    score += 10;
+
+                    // In the case of food consumption, erase the food object
+                    // from the list of bonuses as well as from the canvas
+                    bonusPoints.RemoveAt(n);
+                    paintCanvas.Children.RemoveAt(n);
+                    paintBonus(n);
+                    break;
+                }
+                n++;
             }
         }
     }
