8dbe05ad035c99b5b25719866e9494df114937f6
LucavanderHeide <90605598+LucavanderHeide@users.noreply.github.com>
2021-10-06 13:14:24 +0100

    Add files via upload

---
+++ Project1/Project1/Gamewindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for Gamewindow.xaml
+    /// </summary>
+    public partial class Gamewindow : Window
+    {
+        private ImageBrush PlayerBrush1 = new ImageBrush();
+        private bool MoveRight = false, MoveLeft = false, MoveUp = false, MoveDown = false;
+        private bool MoveRight2 = false, MoveLeft2 = false, MoveUp2 = false, MoveDown2 = false;
+        private DispatcherTimer GameTimer = new DispatcherTimer();
+        const int speed = 10;
+        
+        public Gamewindow()
+        {
+            InitializeComponent();
+
+            GameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            GameTimer.Tick += GameEngine;
+            GameTimer.Start();
+
+            GameCanvas.Focus();
+        }
+
+        /// <summary>
+        /// Hierin staan de bewegingen en de acties de charactar maakt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameEngine(object sender, EventArgs e)
+        {
+            //player 1
+            if (MoveRight)
+            {
+                Canvas.SetLeft(Player1, Canvas.GetLeft(Player1) + speed);
+            }
+            
+            if (MoveLeft)
+            {
+                Canvas.SetLeft(Player1, Canvas.GetLeft(Player1) - speed);
+            }
+
+            if (MoveUp)
+            {
+                Canvas.SetTop(Player1, Canvas.GetTop(Player1) - speed);
+            }
+
+            if (MoveDown)
+            {
+                Canvas.SetTop(Player1, Canvas.GetTop(Player1) + speed);
+            }
+
+            // player 2
+            if (MoveRight2)
+            {
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) + speed);
+            }
+
+            if (MoveLeft2)
+            {
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) - speed);
+            }
+
+            if (MoveUp2)
+            {
+                Canvas.SetTop(Player2, Canvas.GetTop(Player2) - speed);
+            }
+
+            if (MoveDown2)
+            {
+                Canvas.SetTop(Player2, Canvas.GetTop(Player2) + speed);
+            }
+        }
+
+        
+        /// <summary>
+        /// Hierin staat de besturing van de knoppen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            //player 1
+            if (e.Key == Key.A)
+            {
+                MoveLeft = true;
+            }
+
+            if (e.Key == Key.D)
+            {
+                MoveRight = true;
+            }
+
+            if (e.Key == Key.W)
+            {
+                MoveUp = true;
+            }
+
+            if (e.Key == Key.S)
+            {
+                MoveDown = true;
+            }
+
+            
+            //player 2
+            if (e.Key == Key.Left)
+            {
+                MoveLeft2 = true;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                MoveRight2 = true;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                MoveUp2 = true;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                MoveDown2 = true;
+            }
+
+        }
+
+        /// <summary>
+        /// Hierin staat de besturing van de knoppen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameCanvas_KeyUp(object sender, KeyEventArgs e)
+        {
+            //player 1
+            if (e.Key == Key.A)
+            {
+                MoveLeft = false;
+            }
+
+            if (e.Key == Key.D)
+            {
+                MoveRight = false;
+            }
+
+            if (e.Key == Key.W)
+            {
+                MoveUp = false;
+            }
+
+            if (e.Key == Key.S)
+            {
+                MoveDown = false;
+            }
+
+           
+            //player 2
+            if (e.Key == Key.Left)
+            {
+                MoveLeft2 = false;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                MoveRight2 = false;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                MoveUp2 = false;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                MoveDown2 = false;
+            }
+
+
+
+
+        }
+    }
+}
